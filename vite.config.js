ac7690004a097a0560976191f9ccdbf37c0492fc
samuelnoah45 <samuelnoah668@gmail.com>
2022-11-09 11:38:35 +0300

    j

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import gql from 'vite-plugin-simple-gql';
+import graphql from "@rollup/plugin-graphql";
 // import components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),
-    gql(),
+    graphql(),
 
   ],
-  build: {
-    rollupOptions: {
-      external: [
-        "graphql", // 
-        
-      ],
-    },
-  },
+ 
 
 })
